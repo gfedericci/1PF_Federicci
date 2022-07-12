052aecc8d4931f230f8420c6a4103de3a1d79598
Guillermo Federicci <gfedericci@varegos.com>
2022-07-11 23:03:17 -0300

    navegabilidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS}  from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NombreCompletoPipe } from './pipes/nombre-completo.pipe';
+import { TituloDirective } from './directives/titulo.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { TableComponent } from './layout/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NombreCompletoPipe,
+    TituloDirective,
+    ToolbarComponent,
+    NavbarComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-AR'},
+    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
